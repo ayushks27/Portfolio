417a2c1af68fe50964f5e8693e0fe5e6ec1371bd
ayushks27 <purnenduraghavsrivastava@gmail.com>
2025-08-24 02:23:54 +0530

    initial commit

---
+++ src/content_option.js
@@ -0,0 +1,163 @@
+const logotext = "PORTFOLIO";
+const meta = {
+    title: "Purnendu Raghav Srivastava",
+    description: "I’m Purnendu Raghav Srivastava data scientist _ Full stack devloper,currently studying CS Major from JIIT ",
+};
+
+const introdata = {
+    title: "I’m Purnendu Raghav Srivastava",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop web apps",
+    },
+    description: "Hello! I design and build software that works efficiently, reliably, and with purpose.",
+    your_img_url: "./qqe.png",
+};
+
+const dataabout = {
+    title: "About my Self",
+    aboutme: "Hello! I design and build software that works efficiently, reliably, and with purpose. For me, good code is just the beginning, impact is the goal.",
+};
+const worktimeline = [{
+        jobtitle: "Creative Head",
+        where: "Parola - The Literary Hub",
+        date: "2025 - Present",
+    },
+    {
+        jobtitle: "Jaypee Institute of Information Technology",
+        where: "Pre-final year",
+        date: "CGPA : 7.76",
+    },
+    {
+        jobtitle: "Graduation Year",
+        where: "July",
+        date: "2027",
+    },
+];
+
+const skills = [{
+        name: "C++",
+        value: 90,
+    },
+    {
+        name: "Python",
+        value: 75,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "SQL",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Software Development",
+        description: "I’m a Software Developer who loves building applications that combine functionality with great user experience. With expertise in C++ & Python, I focus on writing efficient code and creating solutions that truly make a difference.",
+    },
+    {
+        title: "Web Apps",
+        description: "A Web Application Developer specializing in building responsive, user-friendly, and scalable web applications. I work with technologies such as TypeScript, React, Next.js and Node.js to create seamless digital experiences.",
+    },
+    {
+        title: "Data Analysis",
+        description: "I’m a Data Analyst who loves finding meaning in numbers. I specialize in uncovering patterns, trends, and opportunities hidden within data. By leveraging analytics and visualization tools, I provide meaningful insights that support strategic decision-making.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "Simplicity is the essence of a wise life.",
+        link: "https://the-hidden-hue.vercel.app/",
+    },
+    {
+        img: "https://picsum.photos/400/500/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/ayushks27/uber-ride-cancellation-prediction/tree/main",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "Focus on the vital, discard the trivial.",
+        link: "https://garden-grid.vercel.app/",
+    },
+    {
+        img: "https://picsum.photos/400/650/?grayscale",
+        description: "A wise life is built on essentials, not excess.",
+        link: "https://github.com/ayushks27/drras",
+    },
+    {
+        img: "https://picsum.photos/400/500/?grayscale",
+        description: "Happiness blooms when you focus on what truly counts.",
+        link: "https://drag-iq.vercel.app/",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The simpler your life, the louder your wisdom speaks.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/500/?grayscale",
+        description: "True freedom is living unburdened by the unimportant.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "Let go of the trivial, embrace the vital.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "True wisdom is knowing what to leave behind.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/500/?grayscale",
+        description: "Mastery of life is mastering what truly matters.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Life becomes richer when you strip it to the essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "Happiness comes from keeping only what truly counts.",
+        link: "https://easyjobtracker.netlify.app/",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "purnenduraghavsrivastava@domain.com",
+    YOUR_FONE: "(+91)7905709958",
+    description: "I would be glad to hear from you. For any inquiries or professional opportunities, please feel free to send me a message, and I will respond at the earliest convenience.",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/ayushks27",
+    linkedin: "https://www.linkedin.com/in/purnendu-raghav-1a3289278/",
+    instagram: "https://www.instagram.com/ayush_ks27/",
+    twitter: "https://x.com/Ayushks_27",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
